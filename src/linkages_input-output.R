de4beae1179f39f0080c893b4def881560383604
Nepo Rojas <nepo.rojas@gmail.com>
2025-03-10 12:48:27 -0700

    🚧 Add example 12.1

---
+++ src/linkages_input-output.R
@@ -0,0 +1,8 @@
+# Ejemplo del libro Input-Output Analysis: Foundations and Extensions de Miller y Blair
+# Capítulo 12.1: Supply Side Input–Output Models
+
+Z <- matrix(c(225, 600, 110, 250, 125, 425, 325, 700, 150), nrow=3, byrow=TRUE)
+x <- c(1200, 2000, 1500)
+B <- diag(1/x) %*% Z # ¿Esta es A, la matriz insumo producto? Matriz de coeficiente de salida directa
+G <- solve(diag(3) - B) # El modelo de Ghosh
+G_t <- t(G)
