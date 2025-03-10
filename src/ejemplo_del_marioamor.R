d4a6855713e04257c6c7601eb341b783c509dfe1
Nepo Rojas <nepo.rojas@gmail.com>
2025-03-10 10:05:29 -0700

    🚧 Add new setting

--- src/ejemplo_del_marioamor.R
+++ src/ejemplo_del_marioamor.R
@@ -1,6 +1,7 @@
 library(tidyverse)
 
-mip_df <- read_csv("datos/conjunto_de_datos_mip_cdi_ixi_12018.csv", show_col_types = FALSE, col_types = cols(.default = "d", Concepto = "c"))
+mip_df <- read_csv("datos/conjunto_de_datos_mip_cdi_ixi_12018.csv", show_col_types = FALSE, col_types = cols(.default = "d", Concepto = "c")) |>
+  mutate(moderado = `CPrv - Consumo privado` + `CG - Consumo de gobierno` + `P.51b - Formación bruta de capital fijo` + `P.52 - Variación de existencias` + 0.9*`a P.6 - Exportaciones de bienes y servicios`)
 
 all_concepts <- mip_df |>
   pull(Concepto)
@@ -26,7 +27,13 @@ liontief <- diag(20)- insumo_producto
 
 liontief_inv <- solve(liontief)
 
-demanda_final <- mip_df[1:20, 24]|> pull()
+demanda_final <- mip_df[1:20, 24] * c(1.1, rep(1,19))
 
 demanda_intermedia_teo <- liontief_inv%*%demanda_final |>
+  unname()
+
+# Primer escenario
+demanda_final_mod <- mip_df[1:20, 31]|> pull()
+
+demanda_intermedia_teo <- liontief_inv%*%demanda_final_mod |>
   unname()
