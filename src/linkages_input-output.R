7cf2de7feaa43d1bf1248017f66689206af8bbcb
Nepo Rojas <nepo.rojas@gmail.com>
2025-03-11 10:56:39 -0700

    🚧 Add input-output matrix (direct-input coefficients)

--- src/linkages_input-output.R
+++ src/linkages_input-output.R
@@ -4,5 +4,6 @@
 Z <- matrix(c(225, 600, 110, 250, 125, 425, 325, 700, 150), nrow=3, byrow=TRUE)
 x <- c(1200, 2000, 1500)
 B <- diag(1/x) %*% Z # ¿Esta es A, la matriz insumo producto? Matriz de coeficiente de salida directa
+A <- t(t(Z) / x)
 G <- solve(diag(3) - B) # El modelo de Ghosh
 G_t <- t(G)
