5ea3e33a2426d8eb09994426fa20e13a68df01d4
Nepo Rojas <nepo.rojas@gmail.com>
2025-03-11 15:38:27 -0700

    🛑🧪😇  Test `change_exports()`

--- tests/testthat/test_build_case.R
+++ tests/testthat/test_build_case.R
@@ -9,3 +9,20 @@ describe("set up", {
     expect_equal(obtained_name, expected_name)
   })
 })
+
+demand <- tibble::tibble(
+  "demanda_final" = c(2,4,6),
+  "cprv" = c(1,2,3),
+  "cg" = c(0,0,0),
+  "fbkf" = c(0,0,0),
+  "existencias" = c(0,0,0),
+  "exportaciones" = c(1,2,3)
+)
+
+describe("Change final demands", {
+  it("change exports", {
+    expected_demand <- c(2, 4, 6)
+    obtained_demand <- change_exports(demand, 1.0)
+    expect_equal(obtained_demand, expected_demand)
+  })
+})
