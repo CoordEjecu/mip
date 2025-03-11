9fac0cc79e1777e82c40bbc49eef785e25d787ff
Nepo Rojas <nepo.rojas@gmail.com>
2025-03-11 12:40:16 -0700

    ✅ Pass test

---
+++ R/build_case.R
@@ -0,0 +1,5 @@
+get_industry_name <- function(metadata) {
+  index <- metadata$resources$schema$fields[[1]]$class == "industry"
+  industry_name <- metadata$resources$schema$fields[[1]]$name[index]
+  return(industry_name)
+}
