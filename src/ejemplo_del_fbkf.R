e3ba504a873eb965d7a07b34e5faad740103fc7c
Nepo Rojas <nepo.rojas@gmail.com>
2025-03-12 14:22:01 -0700

    🚧 Add first version of fbkf example

---
+++ src/ejemplo_del_fbkf.R
@@ -0,0 +1,48 @@
+library(tidyverse)
+library(jsonlite)
+library(mip)
+json_file <- "/workdir/datos/datatable.json"
+metadata <- fromJSON(json_file)
+
+mip_df <- read_csv("datos/conjunto_de_datos_mip_cdi_ixi_12018.csv", show_col_types = FALSE, col_types = cols(.default = "d", concepto = "c"))
+
+all_concepts <- mip_df |>
+  pull(concepto)
+
+industrias <- get_industry_name(metadata)
+
+mip_industrias <- mip_df |>
+  select(all_of(industrias))
+
+clean_mip_industrias <- mip_industrias[1:20, ]
+produccion <- all_concepts[32]
+
+total_produccion <- mip_df |>
+  filter(concepto == produccion)
+
+demanda_intermedia <- total_produccion[, 4:23] |> as.numeric()
+
+transacciones_intermedias <- mip_industrias[1:20, ]
+
+insumo_producto <- t(t(transacciones_intermedias) / demanda_intermedia)
+
+liontief <- diag(20) - insumo_producto
+
+liontief_inv <- solve(liontief)
+
+just_industries_rows <- mip_df[1:20, ]
+# Original
+demanda_final <- mip::change_exports(just_industries_rows, 1.0)
+demanda_intermedia <- liontief_inv %*% demanda_final
+
+# Primer escenario
+demanda_final_settup_1 <- mip::change_exports(just_industries_rows, 0.90)
+demanda_intermedia_1 <- liontief_inv %*% demanda_final_settup_1
+percentaje_changed_1 <- mip::percentaje_changed(demanda_intermedia, demanda_intermedia_1)
+
+column_names <- mip::get_industry_column_name(metadata)
+results <- tibble::tibble(
+  "escenarioa" = as.numeric(percentaje_changed_1),
+  "concepto" = column_names
+) |>
+  write_csv("/workdir/reports/tables/changed_percentaje_fbkf.csv")
